939a2c285c94c07500130e5452be7fb406377ab5
unknown <judyvicm1@gmail.com>
2023-09-03 12:36:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Pricing from './pages/Pricing';
 
 function App() {
+  // Determine which component to render based on window.location.pathname
+  let Component;
+  switch (window.location.pathname) {
+    case '/':
+      Component = Home;
+      break;
+    case '/pricing':
+      Component = Pricing;
+      break;
+    case '/about':
+      Component = About;
+      break;
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Component />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+export default function Navbar(){
+    return(
+    <nav className="Nav">
+<a href="/" className="Sitetitle">Site Name</a>
+<ul>
+    <li>
+        <a href="/pricing">Pricing</a>
+        
+    </li>
+    <li>
+        <a href="/about">About</a>
+    </li>
+</ul>
+    </nav>
+    )
+}
